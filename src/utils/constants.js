e0f0f97bc02839e3b6d6b6c8df798266d71519a9
luvnft <140175751+luvnft@users.noreply.github.com>
2024-12-25 18:42:52 -0500

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,9 +1,9 @@
 // export const BASEURL = 'http://localhost:5055'
-export const BASEURL = 'https://brc20-presale.onrender.com'
+export const BASEURL = 'https://ido.luvnft.com'
 export const NETWORKNAME = 'testnet'
 export const DECIMAL = 6;
 export const DECIMAL_8 = 8;
-export const TOKEN_NAME = 'GOLD'
+export const TOKEN_NAME = '$RNT'
 export const SALE_TYPE = {
   public: 'public',
   whitelist: 'whitelist'
